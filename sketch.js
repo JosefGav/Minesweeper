19b058af8e5c0e90731e955870f1ff99d16a47aa
JosefGav <101528981+JosefGav@users.noreply.github.com>
2022-05-03 15:39:10 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,21 +4,28 @@ let bomb;
 let time = 0;
 let flags = 0;
 let bombs = 0;
+let gridDimension; 
+let gridSize = 16
+let numBombs;
+let safeRadius
 
 //keeps track of what stage of the game we are in. For example, at the start of the game (when game state is 0), uppon clicking any tile a small safe area will be revealed.
 let gameState = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  
+  gridDimension = width/gridSize;
+  
   canvas.oncontextmenu = function (e) {
     e.preventDefault();
   };
   textAlign(CENTER, CENTER);
   imageMode(CENTER);
-  textFont("fontBold", 30);
+  textFont("fontBold", gridDimension);
 
   //creates a map array to store the position of the tiles and their corresponding information
-  map = minesInRange(makeMap());
+  map = makeMap();
 
   //so that the tiles dont have a border
   //noStroke();
@@ -29,15 +36,15 @@ function setup() {
   flag = loadImage("flag.png");
   bomb = loadImage("7e09e27489ea5025b6e10befdd2baf3a.png");
   imageMode(CENTER);
-  for (let i = 0; i < 10; i++) {
-    for (let n = 0; n < 10; n++) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let n = 0; n < gridSize; n++) {
       if (i % 2 === 0) {
         if (n % 2 === 0) {
-          square(n * 40, i * 40, 40);
+          square(n * gridDimension, i * gridDimension, gridDimension);
         }
       } else {
         if (n % 2 != 0) {
-          square(n * 40, i * 40, 40);
+          square(n * gridDimension, i * gridDimension, gridDimension);
         }
       }
     }
@@ -51,7 +58,7 @@ function draw() {
     document.getElementById("flagCount").innerHTML = flags;
   }
 
-  if (bombs === 0) {
+  if (bombs === 0 && gameState != 0) {
     gameState = 2;
     document.getElementById("flagCount").innerHTML = flags;
     stroke("black");
@@ -64,7 +71,10 @@ function draw() {
     fill("black");
     text("YOU LOOSSSSSE!!!", width / 2, height / 2);
   }
-  console.log(bombs);
+  
+  if (gameState === 0) {
+    
+  }
 }
 
 //creates the map array
@@ -76,22 +86,74 @@ function makeMap() {
   let indexX;
   let indexY;
 
-  for (let i = 0; i < 10; i++) {
-    for (let c = 0; c < 10; c++) {
-      push = [0, 0, 0, 0];
+  for (let i = 0; i < gridSize; i++) {
+    for (let c = 0; c < gridSize; c++) {
+      push = [0, 0, 0, 0,1];
       row.push(push);
     }
     map.push(row);
     row = [];
   }
+  return map;
+}
 
+function createSafeSpace(x,y) {
+  
+  if (gridSize === 10) { 
+    safeRadius = 1;
+  } else if (gridSize === 16) { 
+    safeRadius = 2;
+  } else if (gridSize === 20) { 
+    safeRadius = 3;
+  } 
+  
+  if (
+      (x === 0 || x === gridSize - 1) &&
+      (y === 0 || y === gridSize - 1) && 
+       gridSize != 10
+      ) {
+    
+      safeRadius ++;
+  }
+  
+  for (let i = 0;i<=safeRadius;i++) {
+    for (let n = 0;n<=safeRadius;n++) {
+      
+      try { 
+        map[y+i][x+n][4] = 0;
+      } catch {}
+      
+      try { 
+        map[y-i][x-n][4] = 0;
+      } catch {}
+      
+      try { 
+        map[y+i][x-n][4] = 0;
+      } catch {}
+      
+      try { 
+        map[y-i][x+n][4] = 0;
+      } catch {}
+      
+    }
+    
+  }
+}
+
+function populateMap(map) {
   let i = 0;
+  
+  if (gridSize === 10) numBombs = 10;
+  if (gridSize === 16) numBombs = 40;
+  if (gridSize === 20) numBombs = 80;
+  
+  while (i != numBombs) {
+    indexX = Math.floor(random(0, gridSize-1));
+    indexY = Math.floor(random(0, gridSize-1));
 
-  while (i != 10) {
-    indexX = Math.floor(random(0, 9));
-    indexY = Math.floor(random(0, 9));
-
-    if (map[indexY][indexX][0] === 0) {
+    if (map[indexY][indexX][0] === 0 &&
+       map[indexY][indexX][4] === 1
+       ) {
       map[indexY][indexX][0] = 1;
       flags++;
       bombs++;
@@ -103,8 +165,8 @@ function makeMap() {
 
 //indicates how many bombs are next to any given tile and adds this information to the tiles respective array
 function minesInRange(map) {
-  for (let i = 0; i < 10; i++) {
-    for (let c = 0; c < 10; c++) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let c = 0; c < gridSize; c++) {
       if (map[i][c][0] === 1) {
         try {
           map[i + 1][c + 1][1] += 1;
@@ -140,23 +202,32 @@ function minesInRange(map) {
 function mousePressed() {
   //makes sure the users click was on the canvas
   if (mouseX <= width && mouseY <= height && gameState != 2) {
+     if (gameState === 0) {
+        createSafeSpace(Math.floor(mouseX / gridDimension),Math.floor(mouseY / gridDimension)) 
+        map = minesInRange(populateMap(map))
+        console.log(map)
+        gameState = 1; 
+      }
+    
+    
+    
     //changes the colour of the tile when clicked
     if (
-      map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][2] === 0 &&
-      map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] === 0
+      map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][2] === 0 &&
+      map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] === 0
     ) {
       replaceTile(
         "darkkhaki",
         "khaki",
-        Math.floor(mouseX / 40),
-        Math.floor(mouseY / 40)
+        Math.floor(mouseX / gridDimension),
+        Math.floor(mouseY / gridDimension)
       );
       if (mouseButton === RIGHT) {
         replaceTile(
           "darkgreen",
           "green",
-          Math.floor(mouseX / 40),
-          Math.floor(mouseY / 40)
+          Math.floor(mouseX / gridDimension),
+          Math.floor(mouseY / gridDimension)
         );
       }
       //i should check if the tile has a mine b4 changing its color
@@ -164,80 +235,79 @@ function mousePressed() {
     if (mouseButton === RIGHT ) {
       //checks if the tile had been flagged or clicked previously
       if (
-        map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] === 0 &&
-        map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][2] === 0 && 
+        map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] === 0 &&
+        map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][2] === 0 && 
         flags > 0
       ) {
         //sets the 3rd index of the array to 1 to indicate the tile has been flagged
-        map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] = 1;
+        map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] = 1;
         console.log("marked");
 
         //flags tile visually
-        flag.resize(30, 30);
+        flag.resize(gridDimension, gridDimension);
         image(flag, centerForTileX(), centerForTileY());
 
         flags--;
 
-        if (map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][0] === 1)
+        if (map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][0] === 1)
           bombs--;
         console.log(flags);
       } else if (
-        map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] === 1
+        map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] === 1
       ) {
-        map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] = 0;
+        map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] = 0;
         console.log("unmarked");
         flags++;
 
         replaceTile(
           "darkgreen",
           "green",
-          Math.floor(mouseX / 40),
-          Math.floor(mouseY / 40)
+          Math.floor(mouseX / gridDimension),
+          Math.floor(mouseY / gridDimension)
         );
-        if (map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][0] === 1)
+        if (map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][0] === 1)
           bombs++;
       }
     } else if (
       mouseButton === LEFT &&
-      map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][3] != 1
+      map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][3] != 1
     ) {
       //makes sure the tile has not been flagged ^
 
       //checks if the tile has a bomb indicated by the 0th index of the array
-      if (map[Math.floor(mouseY / 40)][Math.floor(mouseX / 40)][0] === 1) {
+      if (map[Math.floor(mouseY / gridDimension)][Math.floor(mouseX / gridDimension)][0] === 1) {
         //checks if the player is on their first turn. if this is true then the mine will be removed from the given position.
-        if (gameState === 0) {
-          gameState = 1;
-          removeMine(Math.floor(mouseX / 40), Math.floor(mouseY / 40));
-          console.log("test");
+//         if (gameState === 0) {
+//           gameState = 1;
+//           removeMine(Math.floor(mouseX / gridDimension), Math.floor(mouseY / gridDimension));
+//           console.log("test");
 
-          floodFill(Math.floor(mouseY / 40), Math.floor(mouseX / 40));
-        } else {
+//           floodFill(Math.floor(mouseY / gridDimension), Math.floor(mouseX / gridDimension));
+//         } else {
           //notifies the payer that they clicked on a mine.
-          bomb.resize(40, 40);
+          bomb.resize(gridDimension, gridDimension);
           image(bomb, centerForTileX(), centerForTileY());
           revealBombs();
           console.log("u lose L");
           gameState = 2;
-        }
-      } else {
-        gameState = 1;
-        floodFill(Math.floor(mouseY / 40), Math.floor(mouseX / 40));
+        } else {
+        floodFill(Math.floor(mouseY / gridDimension), Math.floor(mouseX / gridDimension));
         console.log("gj");
 
         //reveals how many bombs are next to the given tile
         //howManyBombs(Math.floor(mouseY/40),Math.floor(mouseX/40));
       }
     }
-  }
+  console.log(map);
+}
 }
 
 function centerForTileX() {
-  return Math.floor(mouseX / 40) * 40 + 20;
+  return Math.floor(mouseX / gridDimension) * gridDimension + gridDimension/2;
 }
 
 function centerForTileY() {
-  return Math.floor(mouseY / 40) * 40 + 20;
+  return Math.floor(mouseY / gridDimension) * gridDimension + gridDimension/2;
 }
 
 //removes a mine and changes the information of the surrounding tiles.
@@ -279,14 +349,16 @@ function removeMine(x, y) {
 function howManyBombs(y, x) {
   fill("black");
   if (map[y][x][1] != 0) {
-    text(`${map[y][x][1]}`, x * 40 + 20, y * 40 + 20);
+    text(`${map[y][x][1]}`, x * gridDimension + gridDimension/2, y * gridDimension + gridDimension/2);
   }
   fill("darkgreen");
 }
 
 function floodFill(y, x) {
+  
+  console.log('floodFill initiated')
   //Checks if the coordinates are within the map
-  if (y < 0 || y >= map.length || x < 0 || x >= map[y].length) return;
+  if (y < 0 || y >= gridSize || x < 0 || x >= gridSize) return;
 
   //Checks if the tile is already revealed
   if (map[y][x][2] === 1) return;
@@ -311,6 +383,11 @@ function floodFill(y, x) {
   floodFill(y - 1, x);
   floodFill(y, x + 1);
   floodFill(y, x - 1);
+  
+  floodFill(y + 1, x + 1);
+  floodFill(y - 1, x - 1);
+  floodFill(y+1, x - 1);
+  floodFill(y -1, x + 1);
 }
 
 //changes the colour of the tile when clicked
@@ -329,16 +406,16 @@ function replaceTile(col1, col2, x, y) {
       fill(col2);
     }
   }
-  square(x * 40, y * 40, 40);
+  square(x * gridDimension, y * gridDimension, gridDimension);
 
   fill("darkgreen");
 }
 
 function revealBombs() {
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
+  for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
       if (map[y][x][0] === 1) {
-        image(bomb, x * 40 + 20, y * 40 + 20);
+        image(bomb, x * gridDimension + gridDimension/2, y * gridDimension + gridDimension/2);
       }
     }
   }
@@ -346,7 +423,12 @@ function revealBombs() {
 
 function keyPressed() {
   if (key === 'r') {
-    canvas.oncontextmenu = function (e) {
+    restart()
+  }
+}
+
+function restart() {
+  canvas.oncontextmenu = function (e) {
     e.preventDefault();
   };
   bombs = 0;
@@ -355,7 +437,7 @@ function keyPressed() {
   time = 0;
 
   //creates a map array to store the position of the tiles and their corresponding information
-  map = minesInRange(makeMap());
+  map = makeMap();
 
   //so that the tiles dont have a border
   //noStroke();
@@ -364,18 +446,27 @@ function keyPressed() {
   background("green");
   fill("darkgreen");
   
-  for (let i = 0; i < 10; i++) {
-    for (let n = 0; n < 10; n++) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let n = 0; n < gridSize; n++) {
       if (i % 2 === 0) {
         if (n % 2 === 0) {
-          square(n * 40, i * 40, 40);
+          square(n * gridDimension, i * gridDimension, gridDimension);
         }
       } else {
         if (n % 2 != 0) {
-          square(n * 40, i * 40, 40);
+          square(n * gridDimension, i * gridDimension, gridDimension);
         }
       }
     }
   }
-  }
 }
+
+
+function changeSize(size){
+  gridSize = size;
+  gridDimension = width/gridSize;
+  
+  restart();
+}
+
+
